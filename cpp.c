caf160ba762cf7fb8f376c5b50840f77dd8b150f
void-memories <46650369+void-memories@users.noreply.github.com>
2019-11-14 14:03:19 +0530

    Add files via upload

--- cpp.c
+++ cpp.c
@@ -1,30 +1,46 @@
+// C++ program to demonstrate auto-complete feature 
+// using Trie data structure. 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<stdbool.h>
 
-#define ALPHABET_SIZE (26)  
+
+// Alphabet size (# of symbols) 
+#define ALPHABET_SIZE (26) 
+
+// Converts key current character into index 
+// use only 'a' through 'z' and lower case 
 #define CHAR_TO_INDEX(c) ((int)c - (int)'a') 
 
+// trie node 
 struct TrieNode 
 { 
 	struct TrieNode *children[ALPHABET_SIZE]; 
+
+	// isWordEnd is true if the node represents 
+	// end of a word 
 	bool isWordEnd; 
 }; 
- 
+
+// Returns new trie node (initialized to NULLs) 
 struct TrieNode *getNode(void) 
 { 
-	struct TrieNode *pNode = (struct TrieNode*)malloc(sizeof(struct TrieNode)); 
+	struct TrieNode *pNode = (struct TrieNode*)malloc(sizeof(struct TrieNode)); //**************************** 
 	pNode->isWordEnd = false; 
+
 	for (int i = 0; i < ALPHABET_SIZE; i++) 
 		pNode->children[i] = NULL; 
 
 	return pNode; 
 } 
 
+// If not present, inserts key into trie. If the 
+// key is prefix of trie node, just marks leaf node 
 void insert(struct TrieNode *root, char key[]) 
 { 
 	struct TrieNode *pCrawl = root; 
+
 	for (int level = 0; level < strlen(key); level++) 
 	{ 
 		int index = CHAR_TO_INDEX(key[level]); 
@@ -34,9 +50,11 @@ void insert(struct TrieNode *root, char key[])
 		pCrawl = pCrawl->children[index]; 
 	} 
 
+	// mark last node as leaf 
 	pCrawl->isWordEnd = true; 
 } 
- 
+
+// Returns true if key presents in trie, else false 
 bool search(struct TrieNode *root, char key[]) 
 { 
 	int length = strlen(key); 
@@ -50,9 +68,12 @@ bool search(struct TrieNode *root, char key[])
 
 		pCrawl = pCrawl->children[index]; 
 	} 
+
 	return (pCrawl != NULL && pCrawl->isWordEnd); 
 } 
 
+// Returns 0 if current node has a child 
+// If all children are NULL, return 1. 
 bool isLastNode(struct TrieNode* root) 
 { 
 	for (int i = 0; i < ALPHABET_SIZE; i++) 
@@ -61,23 +82,40 @@ bool isLastNode(struct TrieNode* root)
 	return 1; 
 } 
 
+// Recursive function to print auto-suggestions for given 
+// node. 
 void suggestionsRec(struct TrieNode* root, char currPrefix[]) 
 { 
+	// found a string in Trie with the given prefix 
 	if (root->isWordEnd) 
-	{
+	{ 
+		//printf("\n\n");
 		puts(currPrefix);
+		//printf("*****Match Found*****\n\n");
+
+		//cout << currPrefix; 
+		//cout << endl; 
 	} 
+
+	// All children struct node pointers are NULL 
 	if (isLastNode(root)) 
 	{
+		//int len=strlen(currPrefix);
+		//currPrefix[len-1]='\0';
 		return; 
 	}
+int k=0;
 	for (int i = 0; i < ALPHABET_SIZE; i++) 
-	{ 
+	{ k=0;
 		if (root->children[i]) 
 		{ 
+			// append current character to currPrefix string 
+			//currPrefix.push_back(97 + i); 
 			int p=97+i;
 			char c=(char)p;
 			strncat(currPrefix,&c,1);
+
+			// recur over the rest 
 			suggestionsRec(root->children[i], currPrefix); 
 			int len=strlen(currPrefix);
 			currPrefix[len-1]='\0';
@@ -85,93 +123,85 @@ void suggestionsRec(struct TrieNode* root, char currPrefix[])
 	} 
 } 
 
+// print suggestions for given query prefix. 
 int printAutoSuggestions(struct TrieNode* root,const char query[]) 
 { 
 	struct TrieNode* pCrawl = root; 
+
+	// Check if prefix is present and find the 
+	// the node (of last level) with last character 
+	// of given string. 
 	int level; 
 	int n = strlen(query); 
 	for (level = 0; level < n; level++) 
 	{ 
 		int index = CHAR_TO_INDEX(query[level]); 
+
+		// no string in the Trie has this prefix 
 		if (!pCrawl->children[index]) 
 			return 0; 
 
 		pCrawl = pCrawl->children[index]; 
-	}
-	
-    bool isWord = (pCrawl->isWordEnd == true); 
+	} 
 
+	// If prefix is present as a word. 
+	bool isWord = (pCrawl->isWordEnd == true); 
+
+	// If prefix is last node of tree (has no 
+	// children) 
 	bool isLast = isLastNode(pCrawl); 
 
+	// If prefix is present as a word, but 
+	// there is no subtree below the last 
+	// matching node. 
 	if (isWord && isLast) 
 	{ 
 		printf("\n \n");
 		puts(query); 
-		printf("*****MATCH FOUND*****\n\n");
+		printf("*****Match Found*****\n\n");
 		return -1; 
 	} 
 
+	// If there are are nodes below last 
+	// matching character. 
 	if (!isLast) 
 	{ 
 		char prefix[30]; 
 		strcpy(prefix,query);
-		printf("\n\nMATCH FOUND With AUTO COMPLETE:-\n");
+		printf("\n\nMatch Found With Auto Complete:-\n");
 		suggestionsRec(pCrawl, prefix); 
 		return 1; 
 	} 
 	return 1;
 } 
 
+// Driver Code 
 int main() 
 { 
 
 	FILE *fp,*fp2;char word[30];
 	fp=fopen("dict.txt","r");
-	fp2=fopen("dict.txt","a");
+	fp2=fopen("res.txt","w");
 	struct TrieNode* root = getNode(); 
 
 	while(!feof(fp))
 	{
 		fscanf(fp,"%s",word);
 		insert(root,word);
+		fprintf(fp2,"%s\n",word);
 	}
+	printf("enter the word to be searched\n");
 	char srch[30];
-    int y=1,opt;
-    while(y)
-    {
-        printf("enter the word to be searched\n");
-        scanf("%s",srch);
-        int comp = printAutoSuggestions(root, srch); 
+	scanf("%s",srch);
+	int comp = printAutoSuggestions(root, srch); 
 
-        if (comp == -1) 
-        {
-            printf("\nNO OTHER STRINGS FOUND WITH THIS PREFIX\n\n"); 
-        }
-        else if (comp == 0) 
-        {
-            printf("\nNO STRINGS FOUND WITH THIS PREFIX\n\n");
-            printf("Do you want to add this word in dictionary\n\nIF YES , PRESS 1\nIF NO , PRESS 0\n\n");
-            scanf("%d",&opt);
-            while(opt!=0 && opt!=1)
-            {
-                printf("invalid entry\n\nEnter again!\n");
-                scanf("%d",&opt);
-            }
-            if(opt==1)
-            {
-                fprintf(fp2,"%s\n",srch);
-                insert(root,srch);
-            }
 
-        }
-         
-        printf("\nWant to search more words\n\nIF YES , PRESS 1\nIF NO , PRESS 0\n\n");
-        scanf("%d",&y);
-        while(y!=0 && y!=1)
-        {
-            printf("invalid entry\n\nEnter again!\n");
-            scanf("%d",&y);
-        }
-    }
+    if (comp == -1) 
+        printf("No other strings found with this prefix\n\n"); 
+  
+    else if (comp == 0) 
+        printf("No string found with this prefix\n\n"); 
+    
+
 	return 0; 
 } 
